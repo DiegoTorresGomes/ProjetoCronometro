2d71e06303eaca2dd5c4e2bc978088c2f991ac77
DiegoTorresGomes <diegotorresgomes@bol.com.br>
2022-08-15 20:49:20 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+var sec = 0;
+var min = 0;
+var hr = 0;
+
+var interval;
+
+function digt (digito) {
+    if(digito < 10){
+        return ('0' + digito)
+    }else{
+        return (digito)
+    }
+}
+
+
+function start() {
+    time()
+    interval = setInterval (time, 1000);
+}
+
+function pause () {
+    clearInterval(interval);
+
+}
+
+function stop() {
+    clearInterval(interval);
+    sec = 0
+    min = 0
+    document.getElementById('timer').innerText= '00:00:00'
+}
+
+function time () {
+    sec++
+    if(sec == 60){
+        min++;
+        sec = 0;
+        if(min == 60){
+            min = 0
+            hr++
+        }
+    }
+    document.getElementById('timer').innerText= digt(hr) + " : " + digt(min) + " : " + digt(sec);
+}
